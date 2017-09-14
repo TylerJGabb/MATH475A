f29e14e0c9ab53a3e396d45c51d7794275b58f05
tylerjgabb <tylerjgabb@gmail.com>
2017-09-13 23:44:27 -0700

    minor tweak

--- Homework/02/source/problem1.py
+++ Homework/02/source/problem1.py
@@ -1,7 +1,34 @@
 import matplotlib.pyplot as plt
 import math as M
+main()
 
+def main():
+    """
+    The main Function
+    """
+    pos_50_algo_1_list = algo1(50)
+    pos_50_algo_2_list = algo2(50)
 
+    neg_50_algo_1_list = algo1(-50)
+    neg_50_algo_2_list = algo2(-50)
+
+    x = plt.figure(1)
+    plt.plot([abs(x - M.exp(50)) for x in pos_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
+    plt.plot([abs(x - M.exp(50)) for x in pos_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
+    plt.legend(loc='best')
+    plt.title('Error in value at iteration For +50')
+    plt.yscale('log')
+    x.show()
+
+    y = plt.figure(2)
+    plt.plot([abs(x - M.exp(-50)) for x in neg_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
+    plt.plot([abs(x - M.exp(-50)) for x in neg_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
+    plt.legend(loc='best')
+    plt.title('Error in value at iteration For -50')
+    plt.yscale('log')
+    y.show()
+
+    raw_input()
 
 def algo1(x):
     """
@@ -32,33 +59,7 @@ def algo2(x):
 
     return terms
 
-def main():
-    """
-    The main Function
-    """
-    pos_50_algo_1_list = algo1(50)
-    pos_50_algo_2_list = algo2(50)
-
-    neg_50_algo_1_list = algo1(-50)
-    neg_50_algo_2_list = algo2(-50)
-
-    x = plt.figure(1)
-    plt.plot([abs(x - M.exp(50)) for x in pos_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
-    plt.plot([abs(x - M.exp(50)) for x in pos_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
-    plt.legend(loc='best')
-    plt.title('Error in value at iteration For +50')
-    plt.yscale('log')
-    x.show()
-
-    y = plt.figure(2)
-    plt.plot([abs(x - M.exp(-50)) for x in neg_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
-    plt.plot([abs(x - M.exp(-50)) for x in neg_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
-    plt.legend(loc='best')
-    plt.title('Error in value at iteration For -50')
-    plt.yscale('log')
-    y.show()
-
-    raw_input()
 
 
-main()   
+
+   
