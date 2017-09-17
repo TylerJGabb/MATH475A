34191fecf00043b406028d9cadf187aad86328e9
tylerjgabb <tylerjgabb@gmail.com>
2017-09-16 21:07:56 -0700

    updating repo

--- Homework/02/source/problem1.py
+++ Homework/02/source/problem1.py
@@ -1,34 +1,39 @@
 import matplotlib.pyplot as plt
 import math as M
-main()
-
+import sys
+VAL = int(sys.argv[1]);
+ITERS = int(sys.argv[2]);
 def main():
     """
     The main Function
     """
-    pos_50_algo_1_list = algo1(50)
-    pos_50_algo_2_list = algo2(50)
+    if len(sys.argv) < 3:
+        print("Usage: python3 problem1.py POWER ITERATIONS")
+        exit(1)
+ 
+    pos_50_algo_1_list = algo1(VAL)
+    pos_50_algo_2_list = algo2(VAL)
 
-    neg_50_algo_1_list = algo1(-50)
-    neg_50_algo_2_list = algo2(-50)
+    neg_50_algo_1_list = algo1(-VAL)
+    neg_50_algo_2_list = algo2(-VAL)
 
     x = plt.figure(1)
-    plt.plot([abs(x - M.exp(50)) for x in pos_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
-    plt.plot([abs(x - M.exp(50)) for x in pos_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
+    plt.plot([abs(x - M.exp(VAL)) for x in pos_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
+    plt.plot([abs(x - M.exp(VAL)) for x in pos_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
     plt.legend(loc='best')
-    plt.title('Error in value at iteration For +50')
-    plt.yscale('log')
+    plt.title('Error in value at iteration For +' + str(VAL))
+    plt.yscale('log', basex=10);
     x.show()
 
     y = plt.figure(2)
-    plt.plot([abs(x - M.exp(-50)) for x in neg_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
-    plt.plot([abs(x - M.exp(-50)) for x in neg_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
+    plt.plot([abs(x - M.exp(-VAL)) for x in neg_50_algo_1_list],'r--',label="Algo I",linewidth=2.5)
+    plt.plot([abs(x - M.exp(-VAL)) for x in neg_50_algo_2_list],'b--',label="Algo II",linewidth=2.5)
     plt.legend(loc='best')
-    plt.title('Error in value at iteration For -50')
-    plt.yscale('log')
+    plt.title('Error in value at iteration For -' + str(VAL))
+    plt.yscale('log', basex=10);
     y.show()
 
-    raw_input()
+    input()
 
 def algo1(x):
     """
@@ -38,7 +43,7 @@ def algo1(x):
     terms = []
     s = 1
     t = x
-    N_values = list(range(1,201))
+    N_values = list(range(1,ITERS + 1))
     for n in N_values:
         s = s + t
         t = t * x / (n + 1)
@@ -53,12 +58,15 @@ def algo2(x):
     """
     terms = []
     s = 1
-    for n in range(200,0,-1):
-        s = s * (x / n + 1)
+    for n in range(ITERS,0,-1):
+        s = (s * x / n) + 1
         terms.append(s)
 
     return terms
 
+
+
+main()
 
 
 
---
+++ Homework/02/source/problem2.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import math
+VERBOSE = "-v" in sys.argv
+"""
+inf = math.inf
+nan = math.nan
+"""
+
+
+def find_smallest():
+    """
+    Finds eps and returns it
+    """
+    x = 1.0
+    divisions = 0
+    while (x > 0):
+        x_last = x
+        x = x / 2
+        divisions += 1
+        if VERBOSE:
+            print(str.format("Division={0}  Result={1}", divisions, x))
+    return x_last
+
+
+def find_largest():
+    """
+    Finds largest representable number in environment
+    """
+    x = 1.0;
+    stage = 0
+    while (x != math.inf and x != math.nan):
+        x_last = x
+        x = x * 2
+        stage += 1
+        if VERBOSE:
+            print(str.format("Stage={0} Result={1}",stage,x))
+    return x_last
+        
+        
