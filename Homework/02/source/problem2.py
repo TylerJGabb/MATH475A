164e60ef1058b52d66909f624102feec4e8c45b4
tylerjgabb <tylerjgabb@gmail.com>
2017-09-16 21:31:08 -0700

    updating repo again

--- Homework/02/source/problem2.py
+++ Homework/02/source/problem2.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import math
+import matplotlib.pyplot as plt
 VERBOSE = "-v" in sys.argv
 """
 inf = math.inf
@@ -8,13 +9,13 @@ nan = math.nan
 """
 
 
-def find_smallest():
+def find_smallest_closest_to(val):
     """
     Finds eps and returns it
     """
     x = 1.0
     divisions = 0
-    while (x > 0):
+    while (val + x > val):
         x_last = x
         x = x / 2
         divisions += 1
@@ -36,5 +37,19 @@ def find_largest():
         if VERBOSE:
             print(str.format("Stage={0} Result={1}",stage,x))
     return x_last
+
+
+def make_a_plot():
+    values = range(0,50,1)
+    results = []
+    for val in values:
+        results.append(find_smallest_closest_to(val))
+        print('done',val)
+    
+    plt.plot(values,results);
+    plt.show();
+    input()
+
+
         
         
