4cb5d836f3c69180e682b55461e3022b01731be7
tylerjgabb <tylerjgabb@gmail.com>
2018-03-19 15:29:44 -0700

    about to start simping L2

--- Homework/SEM2/03/src/script.py
+++ Homework/SEM2/03/src/script.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 sin = lambda x: np.sin(x)
 cos = lambda x: np.cos(x)
 pi = np.pi
-t0 = 1
+t0 = 5
 T = 2*pi
 solution = lambda t: 0 if (t > t0 or t < 0) else 1
 
@@ -19,28 +19,70 @@ def x_hat(k, N):
     return sum/N
 
 def x_tilda(k):
-    if(k == 0):
-        return t0/T
-    return (1/(2*pi*1j*k))(1 - np.exp(-1j*t0*k))
+    if k == 0:
+        retval = t0/T
+    else:
+        retval = (1/(2*pi*1j*k))*(1 - np.exp(-1j*t0*k))
+    return retval
 
 def p_tilda(k):
-    return abs(x_tilda(k))**2
+    retval = abs(x_tilda(k))**2
+    return retval
 
 def p_hat(k,N):
-    return abs(x_hat(k,N))**2
+    retval = abs(x_hat(k,N))**2
+    return retval
 
-def plot_power_spectrum(N):
-    p_hats = []
+'''
+    p tildas only found once for each k
+    p hats found for each value of N for each k
+'''
+
+def generate_power_data():
+    k_arr = list(range(0,1000))
+    p_hat_10 = []
+    p_hat_100 = []
+    p_hat_1000 = []
     p_tildas = []
-    for k in range(N):
-        p_hats.append(p_hat(k,N))
+    for k in k_arr:
+        p_hat_10.append(p_hat(k,100))
+        p_hat_100.append(p_hat(k,450))
+        p_hat_1000.append(p_hat(k,950))
         p_tildas.append(p_tilda(k))
-    plt.plot(range(N),p)
+    plt.plot(k_arr,p_hat_10,label='p hat 100')
+    plt.plot(k_arr,p_hat_100,label='p hat 450')
+    plt.plot(k_arr,p_hat_1000,label='p hat 950')
+    plt.plot(k_arr,p_tildas,label='p tilda')
+    plt.legend()
+    plt.xlabel('k')
+    plt.grid(True)
+    plt.title('t0={}, Power Data Comparison With p tilda'.format(t0))
+    plt.savefig('power_data_t0={}.png'.format(t0))
+    plt.show()
 
 
-
-
-
+def generate_power_data_differences():
+    k_arr = list(range(0, 1000))
+    p_hat_10 = []
+    p_hat_100 = []
+    p_hat_1000 = []
+    p_tildas = []
+    for k in k_arr:
+        ptil = p_tilda(k)
+        p_hat_10.append(abs(p_hat(k,100)-ptil))
+        p_hat_100.append(abs(p_hat(k,450)-ptil))
+        p_hat_1000.append(abs(p_hat(k,950)-ptil))
+    plt.scatter(k_arr,p_hat_10,label='p hat 100',s=0.5)
+    plt.scatter(k_arr,p_hat_100,label='p hat 450',s=0.5)
+    plt.scatter(k_arr,p_hat_1000,label='p hat 950',s=0.5)
+    plt.legend()
+    plt.xlabel('k')
+    plt.grid(True)
+    plt.ylim([-10E-5,10E-4])
+    plt.title('t0={}, Power Data Differences'.format(t0))
+    plt.savefig('power_data_difference_t0={}.png'.format(t0))
+    plt.show()
+    
 
 def x_approx(N, t):
     # returns functions of t
@@ -56,33 +98,31 @@ def x_approx(N, t):
     sum += t0/(2*pi)
     return sum
 
+
 def max_norm(N):
-    delta = 2*pi/N;
+    delta = 2*pi/N
     if delta > 0.01:
         delta = 0.01
     E = []
-    pts = np.arange(0, 2*pi+delta,delta)
+    pts = np.arange(0, 2*pi,delta)
     for t in pts:
-        E.append(abs(solution(t)-x_approx(N,t)))
+        E.append(abs(solution(t)-x_approx(N, t)))
     return max(E)
 
-def do_max_norm_numerical():
+
+def max_norm_numerical():
     E = []
     N = np.arange(100,2100,100)
     for n in N:
         print(n)
         E.append(max_norm(n))
-    plt.plot(N,E,'k-', label='inf norm')
-    plt.savefig('max_norm.png')
-
+    plt.plot(N,E)
+    plt.savefig('max_norm_data.png')
+    plt.show()
 
 
 def generate_overlay_plot():
     sols = []
-    appr4 = []
-    appr8 = []
-    appr16 = []
-    appr32 = []
     apprs = [([], 2**i) for i in [2, 3, 4, 5]]
     pts = np.arange(0, 2*pi+0.01,0.01)
     for t in pts:
@@ -98,8 +138,24 @@ def generate_overlay_plot():
     plt.savefig('overlay.png')
 
 
+def generate_one_plot(N):
+    sols = []
+    appr = []
+    pts = np.arange(0, 2*pi,0.01)
+    for t in pts:
+        print(t,solution(t),x_approx(N,t))
+        sols.append(solution(t))
+        appr.append(x_approx(N,t))
+    plt.plot(pts,sols,'k-', linewidth=2, label='solution')
+    plt.plot(pts, appr, label='N='+str(N))
+    plt.legend()
+    plt.title('t0=' + str(t0))
+    plt.show()
 
 
+if __name__ == '__main__':
+    generate_power_data()
+    generate_power_data_differences()
 
 
 
