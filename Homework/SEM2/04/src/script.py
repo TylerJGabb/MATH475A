01b784d6dd6f02b483266e9159206a9f862a50e3
tylerjgabb <tylerjgabb@gmail.com>
2018-03-31 15:56:58 -0700

    pushing before going home

--- Homework/SEM2/04/src/script.py
+++ Homework/SEM2/04/src/script.py
@@ -4,5 +4,21 @@ sin = np.sin
 cos = np.cos
 pi = np.pi
 
-C_0 = lambda x: x * sin(pi * x)
-0n8vcb36e3fzjv
+c0 = lambda x: x * sin(pi * x)
+
+
+# For which value of delta does it look like BE converges
+
+
+def generate_euler_D(N, Delta):
+    D = []
+    sub = [1, -2, 1]
+    for i in range(N):
+        if i == 0:
+            row = [-2, 1, *([0] * (N - 3)), 1]
+        elif i == N - 1:
+            row = [1, *([0] * (N - 3)), 1, -2]
+        else:
+            row = [*([0] * (i - 1)), *sub, *([0] * (N - 3 - (i - 1)))]
+        print(row)
+
